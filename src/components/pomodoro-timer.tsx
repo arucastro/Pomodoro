a54b62c0eef7658d7c12673dae2c585a475eb7c5
sandro <sandrosilva9029@gmail.com>
2022-11-21 16:07:49 -0300

    botao de descanso

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -14,9 +14,11 @@ function PomodoroTimer(props: Props) {
   const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
   const [timeCounting, setTimeCounting] = useState(false);
   const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
 
   useEffect(() => {
-    if(working) document.body.classList.add("working");
+    if (working) document.body.classList.add("working");
+    if (resting) document.body.classList.remove("working");
   }, [working]);
 
   useInterval(
@@ -29,11 +31,25 @@ function PomodoroTimer(props: Props) {
   const configureWork = () => {
     setTimeCounting(true);
     setWorking(true);
+    setResting(false);
+    setMainTime(props.defaultPomodoroTime);
+  };
+
+  const configureRest = (long: boolean) => {
+    setTimeCounting(true);
+    setWorking(false);
+    setResting(true);
+    if(long) {
+        setMainTime(props.longRestTime);
+    } else{
+        setMainTime(props.shortRestTime);
+    }
+    
   };
 
   return (
     <div className="pomodoro">
-      <h2>You are: working</h2>
+      <h2>You are: {working ? "Working" : "Resting"}</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
         <Button
@@ -41,8 +57,12 @@ function PomodoroTimer(props: Props) {
           onClick={() => configureWork()}
           className={""}
         />
-        <Button text={"Stop!"} className={""} />
-        <Button text={timeCounting ? "Pause" : "Return"} onClick={()=>setTimeCounting(!timeCounting)} className={""} />
+        <Button text={"Rest"} onClick={()=>configureRest(false)} className={!working && !resting ? "hidden" : ""} />
+        <Button
+          text={timeCounting ? "Pause" : "Return"}
+          onClick={() => setTimeCounting(!timeCounting)}
+          className={""}
+        />
       </div>
       <div className="details">
         <p>Testando detalhes</p>
